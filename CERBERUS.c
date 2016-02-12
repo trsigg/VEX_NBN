da92fe5735ae5157157fbb343a57e26a8b1940fb
trsigg <18tsigg@L-708-1858-21-C.dsdut.org>
2016-02-11 21:01:45 -0700

    **Solenoid
    **Pneumatics

--- CERBERUS.c
+++ CERBERUS.c
@@ -316,9 +316,12 @@ task flywheelStabilization() { //modulates motor powers to maintain constant fly
 task lift() {
 	SensorValue[solenoidOne] = 0;
 	SensorValue[solenoidTwo] = 0;
+
 	while (true) {
 		while (vexRT[liftBtn] == 0) { EndTimeSlice(); }
 		SensorValue[solenoidOne] = 1 - SensorValue[solenoidOne];
+		while (vexRT[liftBtn] == 1) { EndTimeSlice(); }
+		while (vexRT[liftBtn] == 0) { EndTimeSlice(); }
 		SensorValue[solenoidTwo] = 1 - SensorValue[solenoidTwo];
 		while (vexRT[liftBtn] == 1) { EndTimeSlice(); }
 	}
