6316a0ed85ac350dc0d4f94d868264c0c56484fd
Tynan Sigg <trsigg@gmail.com>
2016-03-04 22:57:22 -0700

    Gallabarization complete
    Functional gallaBERUS.

--- Gallabarynn.c
+++ Gallabarynn.c
@@ -1,13 +1,13 @@
 #pragma config(Sensor, dgtl1,  Flywheel,       sensorQuadEncoder)
-#pragma config(Sensor, dgtl3,  leftEncoder,    sensorQuadEncoder)
-#pragma config(Sensor, dgtl5,  rightEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  LEFT,           sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  RIGHT,          sensorQuadEncoder)
 #pragma config(Sensor, dgtl7,  IntakeFeed,     sensorTouch)
 #pragma config(Sensor, dgtl8,  FeedLaunch,     sensorTouch)
 #pragma config(Sensor, dgtl9,  flywheelSwitch, sensorDigitalIn)
-#pragma config(Motor,  port1,           ce,            tmotorVex393_HBridge, openLoop)
-#pragma config(Motor,  port2,           rb,            tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port3,           er,            tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port4,           us,            tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port1,           Fly1,          tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port2,           Fly2,          tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           Fly3,          tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port4,           Fly4,          tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port5,           FRight,        tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port6,           BRight,        tmotorVex393_MC29, openLoop, reversed)
 #pragma config(Motor,  port7,           FLeft,         tmotorVex393_MC29, openLoop)
@@ -25,11 +25,12 @@
 
 #include "Vex_Competition_Includes.c"	//Main competition background code...do not modify!
 
-#define Kp 3	//TO TUNE
-#define Ki 0.001	//TO TUNE
-#define Kd 1.7	//TO TUNE
-
-
+float Kp = 3;	//TO TUNE
+int Error = 0;
+float Ki  = 0.001;	//TO TUNE
+float Integral = 0;
+float Kd = 1.7;	//TO TUNE
+int DeltaE = 0;
 int power = 0;
 float Flyspeed = 0;
 int TargetSpeed = 0;
@@ -38,135 +39,60 @@ int TargetSpeeds[5] = {0, 215, 300, 183, 191};	//ORIGINAL: {0, 205, 300, 183, 19
 //  TargetSpeeds[5] = {Off, Skillz, Long, 1st, 2nd}
 int setpowers[5] = {0, 60, 96, 65, 68};
 TVexJoysticks buttons[5] = {Btn8D, Btn7U, Btn7R, Btn7D, Btn7L};
-
-//driveStraight
-bool driveStraightRunning = false;
-int clicks, rightDirection, leftDirection, drivePower, delayAtEnd, timeout;
-
+int PrevError;
 int Flypower = 0;
 int ToggleFeed = 1;
 int ToggleIntkae = 1;
 int n;
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                          Pre-Autonomous Functions
+//
+// You may want to perform some actions before the competition starts. Do them in the
+// following function.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+void pre_auton()
+{
+  // Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
+  // Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
+  bStopTasksBetweenModes = true;
+
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                                 Autonomous Task
+//
+// This task is used to control your robot during the autonomous phase of a VEX Competition.
+// You must modify the code to add your own robot specific commands here.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
 float P, I, D;
-
-//helper functions region
-int limit(int input, int min, int max) {
-	if (input <= max && input >= min) {
-		return input;
-	}
-	else {
-		return (input > max ? max : min);
-	}
-}
-//end helper functions region
-
-//set functions region
-void setLauncherPower(int power) {
-	power = limit(power, 0, 127);
-	motor[ce] = power;
-	motor[rb] = power;
-	motor[er] = power;
-	motor[us] = power;
-}
-//end set functions region
-
-void pre_auton() { bStopTasksBetweenModes = true; }
-
 
 task motorcontrol()
 {
-	float Error = 0;
-	float PrevError = 0;
-	float Integral = 0;
-	float DeltaE = 0;
-
-	while(true)	{
-		SensorValue[Flywheel] = 0;
-		PrevError = Error;
-		wait1Msec(25);
-
+	SensorValue[Flywheel] = 0;
+	while(1)
+	{
 		Flyspeed = abs(SensorValue[Flywheel]);
 		Error = TargetSpeed - Flyspeed;
 		DeltaE = Error - PrevError;
 		Integral += (Error + PrevError)/2;
-		setLauncherPower(setpower + Kp*Error + Ki*Integral + Kd*DeltaE);
+		power = setpower + Kp*Error + Ki*Integral + Kd*DeltaE;
 
-		//debug
 		P = Kp*Error;
 		I = Ki*Integral;
 		D = Kd*DeltaE;
-	}
-}
 
-task driveStraightTask()
-{
-	driveStraightRunning = true;
-
-	int coeff = 5;
-	int totalClicks = 0;
-	int slavePower = drivePower;
-	int error = 0;
-
-	SensorValue[leftEncoder] = 0;
-	SensorValue[rightEncoder] = 0;
-	clearTimer(driveTimer);
-
-	while (abs(totalClicks) < clicks && time1(driveTimer) < timeout)
-	{
-		setDrivePower(drivePower * leftDirection, slavePower * rightDirection);
-
-		error = -SensorValue[leftEncoder] - SensorValue[rightEncoder];
-
-		slavePower += error / coeff;
-
-		totalClicks += SensorValue[leftEncoder];
-		SensorValue[leftEncoder] = 0;
-		SensorValue[rightEncoder] = 0;
-
-		wait1Msec(100);
-	}
-	setDrivePower(0, 0);
-	wait1Msec(delayAtEnd);
-	driveStraightRunning = false;
-}
-
-void driveStraight(int _clicks_, int _leftDirection_, int _rightDirection_, int _drivePower_, int _delayAtEnd_ = 0, bool startAsTask = false, int _timeout_ = 2500) {
-	clicks = _clicks_;
-	rightDirection = _rightDirection_;
-	leftDirection = _leftDirection_;
-	drivePower = _drivePower_;
-	delayAtEnd = _delayAtEnd_;
-	timeout = _timeout_;
-	if (startAsTask) {
-		startTask(driveStraightTask);
-	}
-	else { //runs as function
-		int coeff = 5;
-		int totalClicks = 0;
-		int slavePower = drivePower;
-		int error = 0;
-
-		SensorValue[leftEncoder] = 0;
-		SensorValue[rightEncoder] = 0;
-		clearTimer(driveTimer);
-
-		while (abs(totalClicks) < clicks  && time1(driveTimer) < timeout)
-		{
-			setDrivePower(drivePower * leftDirection, slavePower * rightDirection);
-
-			error = -SensorValue[leftEncoder] - SensorValue[rightEncoder];
-
-			slavePower += error / coeff;
-
-			totalClicks += SensorValue[leftEncoder];
-			SensorValue[leftEncoder] = 0;
-			SensorValue[rightEncoder] = 0;
-
-			wait1Msec(100);
-		}
-		setDrivePower(0, 0);
-		wait1Msec(delayAtEnd);
+		SensorValue[Flywheel] = 0;
+		PrevError = Error;
+		wait1Msec(25);
 	}
 }
 
@@ -175,7 +101,10 @@ task autonomous()
 	TargetSpeed = 950;
 	setpower = 104;
 	startTask(motorcontrol);
-
+	motor[Fly1] = power;
+	motor[Fly2] = power;
+	motor[Fly3] = power;
+	motor[Fly4] = power;
 	wait1Msec(1000);
 	clearTimer(T1);
 	while(true)
@@ -193,6 +122,40 @@ task autonomous()
 	}
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                                 User Control Task
+//
+// This task is used to control your robot during the user control phase of a VEX Competition.
+// You must modify the code to add your own robot specific commands here.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+//task FeedIntake()
+//{
+//	while(1)
+//	{
+//		if(SensorValue[FeedLaunch] == 1)
+//		{
+//			ToggleIntkae = 1;
+//			ToggleFeed = 1;
+//		}
+//		else
+//		{
+//			if(SensorValue[IntakeFeed] == 1)
+//			{
+//				ToggleFeed = 0;
+//				ToggleIntkae = 1;
+//			}
+//			if(SensorValue[Feed2] == 1)
+//			{
+//				ToggleFeed = 1;
+//				ToggleIntkae = 0;
+//			}
+//		}
+//	}
+//}
+
 task usercontrol()
 {
 	SensorValue[LEFT] = 0;
@@ -204,6 +167,10 @@ task usercontrol()
 		Flypower = (power > 0 ? power : 0 );
 		motor[intkae] = (ToggleIntkae == 1 ? (vexRT[Btn6U]*127 + vexRT[Btn6D]*-127) : 127);
 		motor[feed] = (ToggleFeed == 1 ? (vexRT[Btn5U]*127 + vexRT[Btn5D]*-127) : 127);
+		motor[Fly1] = Flypower;
+		motor[Fly2] = Flypower;
+		motor[Fly3] = Flypower;
+		motor[Fly4] = Flypower;
 		motor[BLeft] = vexRT[Ch3];
 		motor[FLeft] = vexRT[Ch3];
 		motor[BRight] = vexRT[Ch2];
